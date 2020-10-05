8014be751cf84d60efa71efb700208b419388ea4
Pushok8 <Pushok8@protonmail.com>
2020-10-05 21:08:02 +0300

    Received data on the number of views of the first fifty ads.

--- parser.py
+++ parser.py
@@ -3,7 +3,9 @@ from requests import Response
 from bs4 import BeautifulSoup
 from typing import List, NewType
 from random import choice, random, randint, shuffle
+from collections import namedtuple
 import time
+
 
 HOST: str = 'https://www.avito.ru/'
 URL: str = HOST + 'elektrogorsk/bytovaya_tehnika/dlya_kuhni-ASgBAgICAURglk8?q=микроволновая+печь'
@@ -12,17 +14,18 @@ USER_AGENTS: List['user_agent'] = [user_agent.strip() for user_agent in open('us
 output_xlsx_file: dict = {
     'Ключ': None,
     'Регион': None,
-    'Общее количество объявлений': None,
-    'Общее количество просмотров всего': None,
-    'Общее количество просмотров за сегодня': None,
-    'Общее количество просмотров за вчера': None,
+    'Общее количество объявлений': 0,
+    'Общее количество просмотров всего': 0,
+    'Общее количество просмотров за сегодня': 0,
+    'Общее количество просмотров за вчера': 0,
     'Дата публикации 10го объявления (сортировка объявлений по дате)': None,
     '20го объявления (сортировка объявлений по дате)': None,
     '50го объявления (сортировка объявлений по дате)': None,
-    'Средняя цена всех со всех объявлений': None,
-    'Общее количество просмотров первых 50 объявлений (сегодня)': None,
-    'Общее количество просмотров первых 50 объявлений (всего)': None
+    'Средняя цена всех со всех объявлений': 0,
+    'Общее количество просмотров первых 50 объявлений (сегодня)': 0,
+    'Общее количество просмотров первых 50 объявлений (всего)': 0
 }
+counter_first_fifty_ads: int = 50
 # Будет расширен функционал
 # ad_name = input('Введите имя объявления: ')
 # region = input('Введите регион: ')
@@ -37,6 +40,8 @@ def set_common_amount_of_ad(bs_html: BeautifulSoup) -> None:
 
 
 def set_total_amount_views(bs_ad_html: BeautifulSoup) -> None:
+    global COUNTER_FIRST_FIFTY_ADS
+
     try:
         views_on_ad_page: str = bs_ad_html.find(class_='title-info-metadata-item').get_text()[1:].split()
     except AttributeError:
@@ -48,19 +53,23 @@ def set_total_amount_views(bs_ad_html: BeautifulSoup) -> None:
 
               Если программа работает нормально, необращайте внимания.
               ''')
+
+    if COUNTER_FIRST_FIFTY_ADS != 0:
+        output_xlsx_file['Общее количество просмотров первых 50 объявлений (всего)'] += int(views_on_ad_page[0])
+        output_xlsx_file['Общее количество просмотров первых 50 объявлений (сегодня)'] += int(views_on_ad_page[1][2:-1])
     output_xlsx_file['Общее количество просмотров всего'] += int(views_on_ad_page[0])
     output_xlsx_file['Общее количество просмотров за сегодня'] += int(views_on_ad_page[1][2:-1])
+    COUNTER_FIRST_FIFTY_ADS -= 1
 
 
-def set_common_amount_of_total_views_and_today(max_pages: int) -> None:
+def send_ad_data_to_functions(max_pages: int) -> None:
     maximum_amount_of_open_links_without_pause: int = randint(1, 5)
     next_page: int = 1
     ad_limit: int = output_xlsx_file['Общее количество объявлений']
 
     while next_page != max_pages:
 
-        page_with_ad: Response = requests.get(URL, headers={'User-Agent': choice(USER_AGENTS)}, params={'p': next_page,
-                                                                                                        's': 104})
+        page_with_ad: Response = requests.get(URL, headers={'User-Agent': choice(USER_AGENTS)}, params={'p': next_page})
         #                                                                                               'q': То что напишет заказчик
         content_of_ad_page: BeautifulSoup = BeautifulSoup(page_with_ad.content, 'html.parser')
         links_on_ads: List[url_type] = [HOST[:-1] + element.get('href') for element in
@@ -82,8 +91,8 @@ def set_common_amount_of_total_views_and_today(max_pages: int) -> None:
             # Get respone page of all pages with ads
             ad_page: Response = requests.get(link, headers={'User-Agent': choice(USER_AGENTS)})
             ad_content: BeautifulSoup = BeautifulSoup(ad_page.content, 'html.parser')
-            set_total_amount_views(ad_content)
             next_page += 1
+            set_total_amount_views(ad_content)
 
         if ad_limit < 50:
             break
@@ -93,7 +102,7 @@ def run():
     avito_page_content: BeautifulSoup = BeautifulSoup(avito_response.content, 'html.parser')
     set_common_amount_of_ad(avito_page_content)
     time.sleep(5)
-    set_common_amount_of_total_views_and_today(2)
+    send_ad_data_to_functions(2)
     print(output_xlsx_file)
 
 
