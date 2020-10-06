366e73238aaf7c6897195860f24d68b3b137dd7d
Pushok8 <Pushok8@protonmail.com>
2020-10-06 20:40:00 +0300

    deleted unnecessary file

--- test.py
+++
@@ -1,9 +0,0 @@
-import openpyxl
-
-wbkName = 'avito_statistic.xlsx'
-wbk = openpyxl.load_workbook(wbkName)
-wks = wbk['avito_statistic']
-someValue = 1337
-wks.cell(row=10, column=1).value = someValue
-wbk.save(wbkName)
-wbk.close
