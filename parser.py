2ca0a2977d452828337c019ec5effa87de2686fe
Pushok8 <Pushok8@protonmail.com>
2020-10-06 10:40:29 +0300

    Added a complete bypass of traps from Avito, and also, distributed the code in its places (principle of single responsibility).

--- parser.py
+++ parser.py
@@ -1,15 +1,19 @@
 import requests
 from requests import Response
 from bs4 import BeautifulSoup
-from typing import List, NewType, NamedTuple, Tuple
-from random import choice, random, randint, shuffle
-from collections import namedtuple
+from typing import List, NewType
+from random import choice, random, randint
+from sys import exit
 import time
 
-
+# CONSTANTS
 HOST: str = 'https://www.avito.ru'
-URL: str = HOST + '/moskovskaya_oblast/avtomobili?q=range+rover'
+URL: str = HOST + '/elektrogorsk/odezhda_obuv_aksessuary/kupit-aksessuary-ASgBAgICAUTeAtoL'
 USER_AGENTS: List['user_agent'] = [user_agent.strip() for user_agent in open('user-agents.txt').readlines()]
+# ANNOTATIONS
+url_type = NewType('url_type', str)
+int_price = NewType('int_price', int)
+# To read the first fifty ads
 counter_first_fifty_ads: int = 50
 # Будет расширен функционал
 # ad_name = input('Введите имя объявления: ')
@@ -29,9 +33,6 @@ output_xlsx_file: dict = {
     'Общее количество просмотров первых 50 объявлений (сегодня)': 0,
     'Общее количество просмотров первых 50 объявлений (всего)': 0
 }
-avito_response: Response = requests.get(URL, headers={'User-Agent': choice(USER_AGENTS)})
-url_type = NewType('url_type', str)
-int_price = NewType('int_price', int)
 
 
 def set_common_amount_of_ad(bs_html: BeautifulSoup) -> None:
@@ -39,56 +40,79 @@ def set_common_amount_of_ad(bs_html: BeautifulSoup) -> None:
     try:
         output_xlsx_file['Общее количество объявлений'] = int(amount_ad.string)
     except AttributeError:
-        print('Если вы видите эту ошибку, то скорее всего Avito заблокировал ваш IP на время.'
-              '\nСледуйте рекомендация, описанные в файле help.txt.')
-        raise AttributeError("'NoneType' object has no attribute 'string'")
+        if bs_html.find('h2', class_='firewall-title') is None:
+            print('По вашим параметрам ничего не найдено.')
+        else:
+            print('Ваш IP адрес заблокировал Avito на время. Следуйте указаниям файла help.txt.')
+        exit()
 
-    
-def set_total_amount_views(bs_ad_html: BeautifulSoup) -> None:
+
+def set_total_amount_views(views_on_ad_page: list) -> None:
     global counter_first_fifty_ads
 
-    try:
-        views_on_ad_page: str = bs_ad_html.find(class_='title-info-metadata-item').get_text()[1:].split()
-    except AttributeError:
-        print('''
-              Это исключение возможно лишь при том, если Avito отвечает не тем сайтом, который парсер 
-              ожидал увидеть. За остальной информацией обращайтесь к файлу help.txt.
-              
-              Если программа продолжает парсить, необращайте внимания.
-              ''')
-    else:
-        print(views_on_ad_page)
-        if counter_first_fifty_ads != 0:
-            output_xlsx_file['Общее количество просмотров первых 50 объявлений (всего)'] += int(views_on_ad_page[0])
-            output_xlsx_file['Общее количество просмотров первых 50 объявлений (сегодня)'] += int(views_on_ad_page[1][2:-1])
-        output_xlsx_file['Общее количество просмотров всего'] += int(views_on_ad_page[0])
-        output_xlsx_file['Общее количество просмотров за сегодня'] += int(views_on_ad_page[1][2:-1])
-    counter_first_fifty_ads -= 1
+    if counter_first_fifty_ads != 0:
+        output_xlsx_file['Общее количество просмотров первых 50 объявлений (всего)'] += int(
+            views_on_ad_page[0])
+        output_xlsx_file['Общее количество просмотров первых 50 объявлений (сегодня)'] += int(
+            views_on_ad_page[1][2:-1])
+        counter_first_fifty_ads -= 1
+    output_xlsx_file['Общее количество просмотров всего'] += int(views_on_ad_page[0])
+    output_xlsx_file['Общее количество просмотров за сегодня'] += int(views_on_ad_page[1][2:-1])
 
 
-def get_price_of_product(bs_ad_html: BeautifulSoup) -> int_price:
-    try:
-        price_of_product = int(str(bs_ad_html.find('span', class_='js-item-price').text).replace(' ', ''))
-    except AttributeError:
-        pass
-    else:
-        return price_of_product
-    return 0
-
-
-def set_average_price_of_all_ads(price_of_all_product: int_price) -> None:
-    output_xlsx_file['Средняя цена всех со всех объявлений'] = \
-        price_of_all_product / output_xlsx_file['Общее количество объявлений']
+def add_price_to_price_from_all_ads(price_of_product: int_price) -> None:
+    output_xlsx_file['Средняя цена всех со всех объявлений'] += price_of_product
 
 
 def set_date_of_publication_of_ad(bs_ad_html: BeautifulSoup) -> None:
     pass
 
 
+def set_average_price_of_all_ads() -> None:
+    output_xlsx_file['Средняя цена всех со всех объявлений'] = \
+        (output_xlsx_file['Средняя цена всех со всех объявлений']
+         / output_xlsx_file['Общее количество объявлений'])
+
+
+def bypass_traps_avito(bs_ad_html: BeautifulSoup, ad_page: Response, link: str) -> int_price:
+
+    try:
+        views_on_ad_page: str = bs_ad_html.find(class_='title-info-metadata-item').get_text()[1:].split()
+        price_of_product = int(str(bs_ad_html.find('span', class_='js-item-price').text).replace(' ', ''))
+        date_of_publication_of_ad = bs_ad_html.find('div', class_='title-info-metadata-item-redesign')
+    except AttributeError:
+        if ad_page.status_code == 404:
+            try:
+                ad_page: Response = requests.get(link, headers={'User-Agent': choice(USER_AGENTS)})
+                bs_ad_html: BeautifulSoup = BeautifulSoup(ad_page.content, 'html.parser')
+                views_on_ad_page: list = bs_ad_html.find(class_='title-info-metadata-item').get_text()[1:].split()
+                price_of_product: int_price = int(str(bs_ad_html.find('span', class_='js-item-price').text).replace(' ', ''))
+            except AttributeError:
+                print('''
+                      Это исключение возможно лишь при том, если Avito отвечает не тем сайтом, который пар-
+                      сер ожидал увидеть. За остальной информацией обращайтесь к файлу help.txt.
+
+                      Если программа продолжает парсить, необращайте внимания.
+                      ''')
+            else:
+                print('Good')
+                set_total_amount_views(views_on_ad_page)
+                add_price_to_price_from_all_ads(price_of_product)
+        elif ad_page.status_code == 429:
+            print('''
+                  Ваш IP был заблокирован на время. Нужно подождать некоторое время, либо зайти на
+                  на сайт через браузер и ввести капчу. Если ничто из этого не помогло, следуйте указа-
+                  ниям в файле help.txt.
+                  ''')
+            exit()
+    else:
+        set_total_amount_views(views_on_ad_page)
+        add_price_to_price_from_all_ads(price_of_product)
+
+
 def send_ad_data_to_functions(max_pages: int) -> None:
 
     maximum_amount_of_open_links_without_pause: int = randint(1, 5)
-    price_all_product: int = 0
     next_page: int = 1
     # Sometimes ads are not enough for one page and Avito fills it with similar ads.
     if output_xlsx_file['Общее количество объявлений'] <= 50:
@@ -107,36 +131,38 @@ def send_ad_data_to_functions(max_pages: int) -> None:
             links_on_ads = [HOST + element.get('href') for element in
                             content_of_ad_page.find_all('a', class_='title-root-395AQ')]
         time.sleep(3)
-        if isinstance(ad_limit, int):
-            shuffle(links_on_ads)
 
         for link in links_on_ads:
-            print(link)
+
             if isinstance(ad_limit, int):
                 ad_limit -= 1
             if ad_limit == 0: break
-            if link[21:link.find(21)] != URL[21:link.find(21)]:
+            if link[21:link[21:].find('/') + 21] != URL[21:URL[21:].find('/') + 21]:
+                print(link, 'is fake link')
                 continue
 
             # It is done to imitate a person, so that Avito does not consider the parser a bot.
+            # If delete this code, Avito can give block by IP for a while.
             if maximum_amount_of_open_links_without_pause == 0:
-                time.sleep(round(randint(6, 8) + random(), 1))
-                maximum_amount_of_open_links_without_pause = randint(1, 5)
+                time.sleep(round(randint(6, 8) + random(), 2))
+                maximum_amount_of_open_links_without_pause = randint(1, 4)
             maximum_amount_of_open_links_without_pause -= 1
 
+            print(link, end=' ')
             ad_page: Response = requests.get(link, headers={'User-Agent': choice(USER_AGENTS)})
-            ad_content: BeautifulSoup = BeautifulSoup(ad_page.content, 'html.parser')
+            bs_ad_content: BeautifulSoup = BeautifulSoup(ad_page.content, 'html.parser')
+            print(ad_page.status_code)
+            bypass_traps_avito(bs_ad_content, ad_page, link)
 
-            set_total_amount_views(ad_content)
-            price_all_product += get_price_of_product(ad_content)
         next_page += 1
-        print('One page is scrapped')
+        print(next_page, 'is next page')
         if isinstance(ad_limit, int):
             break
-    set_average_price_of_all_ads(price_all_product)
+    set_average_price_of_all_ads()
 
 
 def run():
+    avito_response: Response = requests.get(URL, headers={'User-Agent': choice(USER_AGENTS)})
     avito_page_content: BeautifulSoup = BeautifulSoup(avito_response.content, 'html.parser')
     set_common_amount_of_ad(avito_page_content)
     time.sleep(5)
